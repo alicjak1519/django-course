098bab61016bfbd9a8e62cef9b46ded35bc841d3
Alicja <akrzeminska1519@gmail.com>
2020-05-05 17:29:07 +0200

    Start the project

---
+++ fifth_project/app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from app import views
+
+app_name = 'app'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('register/', views.register, name='register'),
+    path('user_login/', views.user_login, name='user_login'),
+    path('special/', views.special, name='special')
+
+]
+
